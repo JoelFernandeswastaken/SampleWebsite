1116229933e2d18c68c1a83b41a6ae3e1a34d99f
CLAYSYS\fernandes.Joel <joel.fernandes@claysys.co>
2023-08-25 13:47:20 +0530

    js form validations

--- public/script.js
+++ public/script.js
@@ -47,3 +47,36 @@ function validate(pattern, text, element, errorMessage){
     }
     errorMessage = "";
 }
+
+function selectOptions(x){
+    let selected = x.value;
+    let options = document.getElementById("city");
+    if (selected === "Goa"){
+        options.innerHTML = "<option value='city-1'>goa-city-1</option><option value='city-2'>goa-city-2</option><option value='city-3'>goa-city-3</option><option value='city-4'>goa-city-4</option><option value='city-5'>goa-city-5</option>";
+    }
+    else if (selected === "Kerala"){
+        options.innerHTML = "<option value='city-1'>kerala-city-1</option><option value='city-2'>kerala-city-2</option><option value='city-3'>kerala-city-3</option><option value='city-4'>kerala-city-4</option><option value='city-5'>kerala-city-5</option>kerala";
+    }
+    else if (selected === "Tamil Nadu"){
+        options.innerHTML = "<option value='city-1'>tamil-nadu-city-1</option><option value='city-2'>tamil-nadu-city-2</option><option value='city-3'>tamil-nadu-city-3</option><option value='city-4'>tamil-nadu-city-4</option><option value='city-5'>tamil-nadu-city-5</option>";
+    }
+    else{
+        options.innerHTML = ""
+    }
+}
+
+function allFieldsFilled(id){
+    let inputs = document.getElementById(id).getElementsByTagName("input");
+    let filled = true;
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+            filled = false;
+            break;
+        }
+    }
+    if (filled) {
+        document.getElementById(id).submit();
+    } else {
+        alert("Please fill all form fields.");
+    }
+}
